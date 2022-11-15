158a82c663146537dde9fc7eab514aaa3ff2425f
KrzysztofLeczycki <92303895+KrzysztofLeczycki@users.noreply.github.com>
2022-11-15 16:25:38 +0100

    Completing data

---
+++ data.py
@@ -0,0 +1,88 @@
+locations = [
+    {'name': 'Northtown', 
+    'connections': [
+        {'name': 'Train station', 'length': 1}, 
+        {'name': 'Redaction', 'length': 1}, 
+        {'name': 'Antique shop', 'length': 1}, 
+        {'name': 'Downtown', 'length': 1}, 
+        {'name': 'Trade District', 'length': 1}
+        ], 
+    'resources':[]},
+    {'name': 'Downtown', 
+    'connections': [
+        {'name': 'Asylum', 'length': 1}, 
+        {'name': 'Bank', 'length': 1}, 
+        {'name': 'The Independence Square', 'length': 1}, 
+        {'name': 'Northtown', 'length': 1}, 
+        {'name': 'Trade District', 'length': 1}, 
+        {'name': 'Easttown', 'length': 1}
+        ], 
+    'resources':[]},
+    {'name': 'Easttown', 
+    'connections': [
+        {'name': 'East Inn', 'length': 1}, 
+        {'name': 'Restaurant', 'length': 1}, 
+        {'name': 'Downtown', 'length': 1}, 
+        {'name': 'Police Station', 'length': 1}, 
+        {'name': 'Rivertown', 'length': 1}
+        ], 
+    'resources':[]},
+    {'name': 'Rivertown', 
+    'connections': [
+        {'name': 'Cementary', 'length': 1}, 
+        {'name': 'Cave', 'length': 1}, 
+        {'name': 'Shop', 'length': 1}, 
+        {'name': 'Easttown', 'length': 1}, 
+        {'name': 'French Hill', 'length': 1}, 
+        {'name': 'Trade District', 'length': 1}
+        ], 
+    'resources':[]},
+    {'name': 'Trade District', 
+    'connections': [
+        {'name': 'Unknown', 'length': 1}, 
+        {'name': 'River Docks', 'length': 1}, 
+        {'name': '?????', 'length': 1}, 
+        {'name': 'Northtown', 'length': 1}, 
+        {'name': 'University', 'length': 1}, 
+        {'name': 'Downtown', 'length': 1},
+        {'name': 'Rivertown', 'length': 1}
+        ], 
+    'resources':[]},
+    {'name': 'University', 
+    'connections': [
+        {'name': 'The Science Building', 'length': 1}, 
+        {'name': 'The Administration Building', 'length': 1}, 
+        {'name': 'Library', 'length': 1}, 
+        {'name': 'Uptown', 'length': 1}, 
+        {'name': 'French Hill', 'length': 1}, 
+        {'name': 'Trade District', 'length': 1}
+        ], 
+    'resources':[]},
+    {'name': 'French Hill', 
+    'connections': [
+        {'name': 'Lodge', 'length': 1}, 
+        {'name': "The Witch's House", 'length': 1}, 
+        {'name': 'University', 'length': 1}, 
+        {'name': 'Rivertown', 'length': 1}, 
+        {'name': 'Southside', 'length': 1}
+        ], 
+    'resources':[]},
+    {'name': 'Southside', 
+    'connections': [
+        {'name': 'Guesthouse', 'length': 1}, 
+        {'name': "Church", 'length': 1}, 
+        {'name': 'Historical Association', 'length': 1}, 
+        {'name': 'French Hill', 'length': 1}, 
+        {'name': 'Uptown', 'length': 1}, 
+        ], 
+    'resources':[]},
+    {'name': 'Uptown', 
+    'connections': [
+        {'name': 'Forest', 'length': 1}, 
+        {'name': "Old Shop", 'length': 1}, 
+        {'name': 'Hospital', 'length': 1}, 
+        {'name': 'Southside', 'length': 1}, 
+        {'name': 'University', 'length': 1}, 
+        ], 
+    'resources':[]},
+]
