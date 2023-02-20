00277ee67426a2cff1579a138be2413e111634c8
Krzysztof-SupComp <92303895+KrzysztofLeczycki@users.noreply.github.com>
2023-02-20 11:54:27 +0100

    Extract resources from locations dict

--- data.py
+++ data.py
@@ -9,25 +9,21 @@ locations = [
         {'name': 'Trade District', 'length': 1}
         ], 
     'resources':[]},
-
     {'name': 'Train station', 
     'connections': [
         {'name': 'Northside', 'length': 1}, 
         ], 
     'resources':['unique items', 'common items']},
-
     {'name': 'Redaction', 
     'connections': [
         {'name': 'Northside', 'length': 1}, 
         ], 
     'resources':['money', 'information']},
-
     {'name': 'Antique shop', 
     'connections': [
         {'name': 'Northside', 'length': 1}, 
         ], 
     'resources':['unique items', 'common items']},
-
     {'name': 'Downtown', 
     'connections': [
         {'name': 'Asylum', 'length': 1}, 
@@ -38,25 +34,21 @@ locations = [
         {'name': 'Easttown', 'length': 1}
         ], 
     'resources':[]},
-
     {'name': 'Asylum', 
     'connections': [
         {'name': 'Downtown', 'length': 1}, 
         ], 
     'resources':['mind health', 'information']},
-
     {'name': 'Bank', 
     'connections': [
         {'name': 'Downtown', 'length': 1}, 
         ], 
     'resources':['money']},
-
     {'name': 'The Independence Square', 
     'connections': [
         {'name': 'Downtown', 'length': 1}, 
         ], 
     'resources':['unique items', 'information']},
-
     {'name': 'Easttown', 
     'connections': [
         {'name': 'East Inn', 'length': 1}, 
@@ -66,25 +58,21 @@ locations = [
         {'name': 'Rivertown', 'length': 1}
         ], 
     'resources':[]},
-
     {'name': 'East Inn', 
     'connections': [
         {'name': 'Easttown', 'length': 1}, 
         ], 
     'resources':['money', 'common items']},
-
     {'name': 'Restaurant', 
     'connections': [
         {'name': 'Easttown', 'length': 1}, 
         ], 
     'resources':['money', 'health']},
-
     {'name': 'Police Station', 
     'connections': [
         {'name': 'Easttown', 'length': 1}, 
         ], 
     'resources':['common items', 'information']},
-
     {'name': 'Rivertown', 
     'connections': [
         {'name': 'Cementary', 'length': 1}, 
@@ -95,25 +83,21 @@ locations = [
         {'name': 'Trade District', 'length': 1}
         ], 
     'resources':[]},
-
     {'name': 'Cementary', 
     'connections': [
         {'name': 'Rivertown', 'length': 1}, 
         ], 
     'resources':['unique items', 'information']},
-
     {'name': 'Cave', 
     'connections': [
         {'name': 'Rivertown', 'length': 1}, 
         ], 
     'resources':['common items', 'spells']},
-
     {'name': 'Shop', 
     'connections': [
         {'name': 'Rivertown', 'length': 1}, 
         ], 
     'resources':['common items', 'money']},
-
     {'name': 'Trade District', 
     'connections': [
         {'name': 'Unknown', 'length': 1}, 
@@ -125,25 +109,21 @@ locations = [
         {'name': 'Rivertown', 'length': 1}
         ], 
     'resources':[]},
-
     {'name': 'Deserted Island', 
     'connections': [
         {'name': 'Trade District', 'length': 1}, 
         ], 
     'resources':['spells', 'information']},
-
     {'name': 'Unknown', 
     'connections': [
         {'name': 'Trade District', 'length': 1}, 
         ], 
     'resources':['unique items', 'information']},
-
     {'name': 'Docks', 
     'connections': [
         {'name': 'Trade District', 'length': 1}, 
         ], 
     'resources':['money', 'common items']},
-
     {'name': 'University', 
     'connections': [
         {'name': 'The Science Building', 'length': 1}, 
@@ -154,25 +134,21 @@ locations = [
         {'name': 'Trade District', 'length': 1}
         ], 
     'resources':[]},
-
     {'name': 'The Science Building', 
     'connections': [
         {'name': 'University', 'length': 1}, 
         ], 
     'resources':['unique items', 'information']},
-
     {'name': 'The Administration Building', 
     'connections': [
         {'name': 'University', 'length': 1}, 
         ], 
     'resources':['money', 'skills']},
-
     {'name': 'Library', 
     'connections': [
         {'name': 'University', 'length': 1}, 
         ], 
     'resources':['unique items', 'spells']},
-
     {'name': 'French Hill', 
     'connections': [
         {'name': 'Lodge', 'length': 1}, 
@@ -182,19 +158,16 @@ locations = [
         {'name': 'Southside', 'length': 1}
         ], 
     'resources':[]},
-
     {'name': "The Witch's House", 
     'connections': [
         {'name': 'French Hill', 'length': 1}, 
         ], 
     'resources':['spells', 'information']},
-
     {'name': 'Lodge', 
     'connections': [
         {'name': 'French Hill', 'length': 1}, 
         ], 
     'resources':['unique items', 'information']},
-
     {'name': 'Southside', 
     'connections': [
         {'name': 'Guesthouse', 'length': 1}, 
@@ -204,25 +177,21 @@ locations = [
         {'name': 'Uptown', 'length': 1}, 
         ], 
     'resources':[]},
-
     {'name': 'Guesthouse', 
     'connections': [
         {'name': 'SouthSide', 'length': 1}, 
         ], 
     'resources':['health', 'allies']},
-
     {'name': 'Church', 
     'connections': [
         {'name': 'SouthSide', 'length': 1}, 
         ], 
     'resources':['mind health']},
-
     {'name': 'Historical Association', 
     'connections': [
         {'name': 'SouthSide', 'length': 1}, 
         ], 
     'resources':['skills', 'spells']},
-
     {'name': 'Uptown', 
     'connections': [
         {'name': 'Forest', 'length': 1}, 
@@ -232,19 +201,16 @@ locations = [
         {'name': 'University', 'length': 1}, 
         ], 
     'resources':[]},
-
     {'name': 'Forest', 
     'connections': [
         {'name': 'Uptown', 'length': 1}, 
         ], 
     'resources':['money', 'common items']},
-
     {'name': 'Old Shop', 
     'connections': [
         {'name': 'Uptown', 'length': 1}, 
         ], 
     'resources':['spells', 'unique items']}, 
-
     {'name': 'Hospital', 
     'connections': [
         {'name': 'Uptown', 'length': 1}, 
@@ -252,8 +218,7 @@ locations = [
     'resources':['health', 'information']}
 ]
 
-resources_list = set()
+# Extract resources from locations dict
+resources = set()
 for location in locations:
-    if location['resources'] is not None:
-        for resource in location['resources']:
-            resources_list.add(resource)
+   resources.update(location['resources'])
